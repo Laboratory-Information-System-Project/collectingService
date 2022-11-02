6b3cc7d38b5a8a60fed95dd3c81df7f6a5e6f7e9
liverbird <ehdgns07@gmail.com>
2022-11-02 17:58:12 +0900

    init

---
+++ src/main/java/com/douzone/collectingservice/CollectingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.douzone.collectingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollectingServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CollectingServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/douzone/collectingservice/CollectingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.douzone.collectingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollectingServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
